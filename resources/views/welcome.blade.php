8db9c69ca8a9d332663c5c050905221ee332c59b
Nikola Rafajlovic <nikolaraf@hotmail.com>
2019-04-01 16:22:59 +0200

    Title change
    Title change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    LaravStart
                 </div>
 
                 <div class="links">
